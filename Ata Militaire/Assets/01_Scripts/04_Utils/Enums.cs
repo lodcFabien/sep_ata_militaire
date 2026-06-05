0d1b4c25b33b901edd1a576162a3c820df8d6a61
Antoine Richard <antoine_r@loeilduchat.fr>
2026-06-05 09:26:17 +0200

    wip 6

---
+++ Ata Militaire/Assets/01_Scripts/04_Utils/Enums.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Enums
+{
+    public enum AppState
+    {
+        MainMenu,
+        Plate,
+        Popup
+    }
+
+    public enum PlateType
+    {
+        Transport,
+        Fighter,
+        Helico,
+        Drone
+    }
+}
